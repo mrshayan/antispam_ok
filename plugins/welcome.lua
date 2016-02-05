22cea1349dc58c1e0e541b58a47703b10188cdd5
mohamadkhoshnava <mohamad.khoshnava@gmail.com>
2016-02-05 17:11:47 +0330

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -58,7 +58,7 @@ local function description_rules(msg, nama)
          rules = data[tostring(msg.to.id)]["rules"]
          rules = "\nRules :\n"..rules.."\n"
       end
-      local sambutan = "Hi "..nama.."\nWelcome to '"..string.gsub(msg.to.print_name, "_", " ").."'\nYou can use /help for see bot commands\n"
+      local sambutan = "سلام "..nama.."\nخوش اومدی به '"..string.gsub(msg.to.print_name, "_", " ").."'\nYou can use /help for see bot commands\n"
       local text = sambutan..about..rules.."\n"
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
@@ -88,13 +88,13 @@ local function run(msg, matches)
       description_rules(msg, nama)
    elseif matches[1] == "chat_del_user" then
        local bye_name = msg.action.user.first_name
-       return 'Sikout '..bye_name
+      return 'بسلامت '..ye_name
    end
 end
 
 return {
    description = "Welcoming Message",
-   usage = "send message to new member",
+   usage = "sازسال پیام به کاربر جدید",
    patterns = {
       "^!!tgservice (chat_add_user)$",
       "^!!tgservice (chat_add_user_link)$",
