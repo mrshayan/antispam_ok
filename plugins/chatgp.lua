6ac7babfc653e10ea7d56131f05985b8a248a130
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-02-06 12:36:43 +0330

    Create chatgp.lua

---
+++ plugins/chatgp.lua
@@ -0,0 +1,71 @@
+
+do
+
+function run(msg)
+if msg.text == "سلام" then
+	return "سلام علیکم"
+end
+if msg.text == "Hi" then
+	return "Hello honey"
+end
+if msg.text == "Hello" then
+	return "Hi bb"
+end
+if msg.text == "hello" then
+	return "Hi honey"
+end
+if msg.text == "Salam" then
+	return "Salam aleykom"
+end
+if msg.text == "salam" then
+	return "va aleykol asalam"
+end
+if msg.text == "zac" then
+	return "Barash bezan sak"
+end
+if msg.text == "Zac" then
+	return "Barash bezan sak"
+end
+if msg.text == "ZAC" then
+	return "Barash bezan sak"
+end
+if msg.text == "رنجر" then
+	return "بله?"
+end
+if msg.text == "رنجر" then
+	return "چیه?"
+end
+if msg.text == "bot" then
+	return "hum?"
+end
+if msg.text == "Bot" then
+	return "Huuuum?"
+end
+if msg.text == "?" then
+	return "Hum??"
+end
+if msg.text == "بای" then
+	return "بسلامت"
+end
+if msg.text == "بای" then
+	return "بای بای"
+end
+end
+
+return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+		"^سلام",
+		"^[Hh]ello$",
+		"^[Zz]ac$",
+		"^ZAC$",
+		"^[Bb]ot$",
+		"^رنجر$",
+		"^بای$",
+		"^?$",
+		"^[Ss]alam$",
+		}, 
+	run = run,
+
+}
